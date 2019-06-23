21927c6ac9dfa5e0fd21a02a3de1066ad7db1b3f
Kaio Pomini <kaiopomini@gmail.com>
2019-06-22 21:33:01 -0300

    update

--- MAPA.cpp
+++ MAPA.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>  // cls();
-#include <cstdlib>  // system ("pause");
-#include <locale.h>  // pt-br
+#include <stdlib.h>
+#include <locale.h> 
 
-#define maximoCad 5  // limite de cadastros
+#define MAX 5  
+
 
 struct livro {
 	int codigo;
@@ -13,42 +13,17 @@ struct livro {
 };
 
 
-int gerarCodigo(int n, livro *cad){  //gera o proximo numero de cadastro (partindo de 1) que não esteja ocupado no array passado por parametro
-	int proxCad = 1;
-	int i;
-	bool igual = true;
-	while(igual == true){
-		igual = false;
-		for(i=0; i<n; i++){
-			if (cad[i].codigo == proxCad){
-				igual = true;
-			}
-		}
-		if (igual == true){
-			proxCad += 1;
-		}
-	};
-	return proxCad;
-};
-
-
 int main(){
-	livro cadastro [maximoCad];
-	int count, opc, i, tamanhoVetor;
+	livro cadastro [MAX];
+	int count, opc, i;
 	bool achou;
 	count = 0;
-	opc = -1;
-	
-	tamanhoVetor = sizeof(cadastro)/sizeof(cadastro[0]);
-	
+		
 	setlocale(LC_ALL, "Portuguese");
 	
-	while (opc != 0){
+	do{
 		
-		printf("-------------------------------\n");
-		printf("--------- CADBOOK 1.0 ---------\n");
-		printf("-------------------------------\n\n");		
-		printf("\n1 - CADASTRAR LIVRO");
+		printf("1 - CADASTRAR LIVRO");
 		printf("\n2 - LISTAR LIVROS");
 		printf("\n0 - SAIR");
 		printf("\n\n\nINFORME A OPÇÃO DESEJADA: ");
@@ -56,18 +31,16 @@ int main(){
 		fflush(stdin);
 		system("cls");
 		
-		if (opc == 0){
+		switch (opc){
 			
-			printf("OBRIGADO POR USAR O CADBOOK\n\n");
-			system("pause");
-			
-		} else {
-			
-			if (opc == 1){  // cadastrar
+			case 0:
+				printf("TENHA UM BOM DIA!!\n\n");
+				system("pause");
+				break;
 				
-				if (count < maximoCad){	 // Máximo de cadastros 
-							
-					cadastro[count].codigo = gerarCodigo(tamanhoVetor, cadastro);  // gerar novo codigo não repetido, passando como parametro o tamanho do Array e o Array
+			case 1:  	
+				if (count < MAX){	 				
+					cadastro[count].codigo = count+1;
 					fflush(stdin);
 					printf("Digite o titulo do livro: ");
 					gets(cadastro[count].titulo);
@@ -75,49 +48,48 @@ int main(){
 					gets(cadastro[count].autor);
 					printf("\nDigite o nome da editora: ");
 					gets (cadastro[count].editora);
-					count += 1;  // contador de cadastros realizados
+					count += 1;  
 					printf("\n\n\nCadastro realizado com sucesso!!\n\n");
 					fflush(stdin);
 					system("pause");
-		
-				} else {
-					
+			
+				} else {	
 					printf("Sistema de cadastro lotado. Não é possível armazenar mais informações!\n\n");
 					system("pause");
+						
+				}
+
+				break;
+			
+			case 2:
+				if (count > 0){	
+					for (i=0; i<count; i++){						
+						printf("CODIGO: %d", cadastro[i].codigo );
+						printf("\nTITULO: %s", cadastro[i].titulo);
+						printf("\nAUTOR: %s", cadastro[i].autor);						
+						printf("\nEDITORA: %s\n\n", cadastro[i].editora);
+								
+					}
+							
+				} else {  		
+					printf("Lista vazia!\n\n");
 					
 				}
-			
-			} else {
-				if (opc == 2){  // mostrar todos os cadastros
-					if (count > 0){
-					
-						for (i=0; i<count; i++){
-										
-							printf("CODIGO: %d", cadastro[i].codigo );
-							printf("\nTITULO: %s", cadastro[i].titulo);
-							printf("\nAUTOR: %s", cadastro[i].autor);						
-							printf("\nEDITORA: %s\n\n", cadastro[i].editora);
-							
-						}
 						
-					} else {  // erro de lista vazia
-						
-						printf("Lista vazia!\n\n");
-					}
-					
-					system("pause");
-						
-				} else {  
+				system("pause");
+				break;
 				
+			default:			
 					printf("ERRO! OPÇÃO INVÁLIDA!\n\n");
 					system("pause");
-					
-				}
-			}	
+						
+				break;					
+			
 		}
 		
 		system("cls");
 		
-	};
+	} while (opc != 0);
+	
 	return(0);
 }
